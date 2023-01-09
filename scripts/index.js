c87d13c75edae74bdc891909f94951ab0ea67a4a
MaksimParshin <mak.yp@yandex.ru>
2023-01-09 17:36:19 +0300

    add new popup add genirate cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,96 @@
-let username = document.querySelector(".profile__name");
-let profession = document.querySelector(".profile__profession");
-let editButton = document.querySelector(".profile__edit-button");
-let closeButton = document.querySelector(".popup__close-button");
-let inputName = document.querySelector(".popup__input_value_username");
-let inputProfession = document.querySelector(".popup__input_value_profession");
-let submitForm = document.querySelector(".popup__form");
-let popup = document.querySelector(".popup");
+const username = document.querySelector(".profile__name");
+const profession = document.querySelector(".profile__profession");
+const editButton = document.querySelector(".profile__edit-button");
+const closeButtons = document.querySelectorAll(".popup__close-button");
+const inputName = document.querySelector(".popup__input_value_username");
+const inputProfession = document.querySelector(
+  ".popup__input_value_profession"
+);
+const addButton = document.querySelector('.profile__add-button');
 
-function openPopup() {
-  popup.classList.add("popup_opend");
-  inputName.value = username.textContent;
-  inputProfession.value = profession.textContent;
+const submitForm = document.querySelector(".popup__form");
+const popup = document.querySelector(".popup");
+const popupProfile = document.querySelector('.popup_name_profile')
+const popupElement = document.querySelector('.popup_name_element')
+const likeButton = document.querySelector(".element__like-button");
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+const elementTemplate = document.querySelector(".element-template").content;
+const sectionElements = document.querySelector(".elements");
+
+function openPopup(newPopup) {
+  newPopup.classList.add("popup_opend");
+
 }
 
-function closePopup() {
-  popup.classList.remove("popup_opend");
+function closePopup(newPopup) {
+  newPopup.classList.remove("popup_opend");
 }
 
 function handleFormSubmit(e) {
   e.preventDefault();
   username.textContent = inputName.value;
   profession.textContent = inputProfession.value;
-  closePopup();
+  closePopup(popupProfile);
 }
 
-editButton.addEventListener("click", openPopup);
-closeButton.addEventListener("click", closePopup);
+
+
+function hasLike() {
+  likeButton.classList.toggle("element__like-button_state_active");
+}
+
+function loadCards() {
+  for (let i = 0; i < initialCards.length; i++) {
+    const element = elementTemplate.querySelector('.element').cloneNode(true)
+    const image = element.querySelector('.element__image').src = initialCards[i].link;
+    const text = element.querySelector('.element__title').textContent = initialCards[i].name;
+    sectionElements.append(element)
+  }
+}
+
+loadCards()
+
+
+
+editButton.addEventListener("click", function (){
+  openPopup(popupProfile);
+  inputName.value = username.textContent;
+  inputProfession.value = profession.textContent;
+});
+
+addButton.addEventListener("click", function (){
+  openPopup(popupElement);
+});
+
+closeButtons.forEach(e=> e.addEventListener("click", function (e) {
+  let close = e.target.closest('.popup')
+  closePopup(close);
+} ));
+
 submitForm.addEventListener("submit", handleFormSubmit);
