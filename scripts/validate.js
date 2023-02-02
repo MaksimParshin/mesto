d06522964a4f58eb8783c0664306bf8a2c8e5361
MaksimParshin <mak.yp@yandex.ru>
2023-02-02 17:54:39 +0300

    added two function validate

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,1 +1,14 @@
-const forms = document.querySelectorAll('.popup');
+
+function showInputError(formElement, inputElement, errorMessage, obj) {
+  const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
+  inputElement.classList.add(obj['inputErrorClass']);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(obj['errorClass']);
+}
+
+function hideInputError(formElement, inputElement, obj) {
+  const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
+  inputElement.classList.remove(obj['inputErrorClass']);
+  errorElement.classList.remove(obj['errorClass']);
+  errorElement.textContent = '';
+}
