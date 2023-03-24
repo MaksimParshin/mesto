5107ee8716ae06b7c7c920359c3d41d46e322d32
MaksimParshin <mak.yp@yandex.ru>
2023-03-24 22:54:22 +0300

    feat succsesful post card to the server

--- src/components/Api.js
+++ src/components/Api.js
@@ -52,16 +52,91 @@ export default class Api {
   }
 
   setAvatar(item) {
-    return fetch(`https://mesto.nomoreparties.co/v1/${this._userID}/users/me/avatar`, {
-      method: "PATCH",
+    return fetch(
+      `https://mesto.nomoreparties.co/v1/${this._userID}/users/me/avatar`,
+      {
+        method: "PATCH",
+        headers: {
+          authorization: this._token,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          avatar: item.avatar,
+        }),
+      }
+    ).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    });
+  }
+  createCard(data) {
+    return fetch(`https://mesto.nomoreparties.co/v1/${this._userID}/cards`, {
+      method: "POST",
       headers: {
         authorization: this._token,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        avatar: item.avatar
+        name: data.name,
+        link: data.link,
       }),
     }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    });
+  }
+
+  deleteCard(cardId) {
+    return fetch(
+      `https://mesto.nomoreparties.co/v1/${this._userID}/cards/${cardId}`,
+      {
+        method: "DELETE",
+        headers: {
+          authorization: this._token,
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    });
+  }
+
+  putLike(cardId) {
+    return fetch(
+      `https://mesto.nomoreparties.co/v1/${this._userID}/cards/${cardId}/likes`,
+      {
+        method: "PUT",
+        headers: {
+          authorization: this._token,
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    });
+  }
+
+  deleteLike(cardId) {
+    return fetch(
+      `https://mesto.nomoreparties.co/v1/${this._userID}/cards/${cardId}/likes`,
+      {
+        method: "DELETE",
+        headers: {
+          authorization: this._token,
+          "Content-Type": "application/json",
+        },
+      }
+    ).then((res) => {
       if (res.ok) {
         return res.json();
       }
